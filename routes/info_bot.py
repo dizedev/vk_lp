2da92f352f5a9c3e9439796b0615ec04bb61a357
dize_dev <destersamp@gmail.com>
2023-10-29 23:50:29 +0600

    Добавлена информация о боте

--- routes/info_bot.py
+++ routes/info_bot.py
@@ -3,9 +3,9 @@ from vkbottle.user import Message
 from config import bl, edit_message
 
 
-@bl.message(Permission(), text="хелло")
+@bl.message(Permission(), text="бот")
 async def hello(message: Message):
     await edit_message(
         message,
-        "привет"
+        "Информация о боте 🤖:\n\n Название бота 🤖: dize_lp\n Версия бота ⚙️: v0.1\n Создатель бота 👨‍🦱: @dize_dev(Илдырым Денница)."
     )
