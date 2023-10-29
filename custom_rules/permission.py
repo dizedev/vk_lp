e35a6879910ac956ef5fd741d3f2c3e8a8a5cdd9
Alex <me@chydo.dev>
2023-10-30 00:46:41 +0700

    some edits

--- custom_rules/permission.py
+++ custom_rules/permission.py
@@ -3,21 +3,19 @@ from vkbottle.user import Message
 import json
 
 
+def load_trusted_ids():
+    try:
+        with open("./user_ids.json", "r") as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError:
+        # Если файл не существует, вернуть пустой список
+        return []
+
 
 class Permission(ABCRule[Message]):
     def __init__(self):
-        self.trusted_ids = self.load_trusted_ids()
-
-    def load_trusted_ids(self):
-        try:
-            with open("./user_ids.json", "r") as json_file:
-                data = json.load(json_file)
-                return data
-        except FileNotFoundError:
-            # Если файл не существует, вернуть пустой список
-            return []
+        self.trusted_ids = load_trusted_ids()
 
     async def check(self, event: Message) -> bool:
         return event.from_id in self.trusted_ids
-
-
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,3 @@
-from . import game_handler, deletedmsg, info_bot
+from . import game_handler, delete_msg, info_bot
 
 labelers = [game_handler.bl, deletedmsg.bl, info_bot.bl]
