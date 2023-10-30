20d1b083442b7b69371ae756b288a0f139ad6466
dize_dev <destersamp@gmail.com>
2023-10-30 21:52:46 +0600

    fixed bug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from config import user
 from routes import labelers
 from loguru import logger
 
+
 logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
 
 for custom_labeler in labelers:
