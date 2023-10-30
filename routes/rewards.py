f96505a675c436ed5012c8ca67c3ef1e5c1f3c05
dize_dev <destersamp@gmail.com>
2023-10-30 18:05:42 +0600

    Обновление: v0.2

--- routes/rewards.py
+++ routes/rewards.py
@@ -10,4 +10,4 @@ listToStr = ' '.join(map(str, rewards_list))
 @bl.message(Permission(), text=[prefix_bot + " достижения"])
 async def rewards(message: Message):
     await edit_message(message,
-                       "Вот список всех достижений 🏆: \n " + listToStr)
+                       "Вот список всех достижений🏆: \n " + listToStr)
