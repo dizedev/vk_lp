972b8e94d735354da41a373a6dc01490a3f1623b
dize_dev <destersamp@gmail.com>
2023-10-30 21:50:47 +0600

    fixed bug

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,3 @@
-from . import game_handler, deletedmsg, info_bot, rewards
+from . import game_handler, deletedmsg, info_bot, rewards, not_command
 
-labelers = [game_handler.bl, deletedmsg.bl, info_bot.bl, rewards.bl]
+labelers = [game_handler.bl, deletedmsg.bl, info_bot.bl, rewards.bl, not_command.bl]
