9c691d8bf099e0b50e4137829ec2cfd06cc9c2a0
dize_dev <destersamp@gmail.com>
2023-10-31 22:11:13 +0600

    Обновление v0.5.2

--- routes/info_bot.py
+++ routes/info_bot.py
@@ -1,22 +1,23 @@
-import asyncio
-
 from custom_rules.permission import Permission
 from vkbottle.user import Message
 from config import bl, edit_message, prefix_bot, user
-from utils import rewards_list
 
 
 @bl.message(Permission(), text=[prefix_bot + " бот"])
 async def about_bot(message: Message):
     peer_id = message.peer_id
 
-    await edit_message(message,
-                       f"Информация о боте 🤖:\n\n Название бота 🤖: Impulse LP\n Версия бота ⚙️: v0.1\n Создатель бота 👨‍🦱: @dize_dev(Илдырым Денница)\n Достижения🏆: {rewards_list[1]}")
-    await asyncio.sleep(0.5)
+    # await edit_message(message,
+    #                    f"Информация о боте 🤖:\n\n Название бота 🤖: Impulse LP\n c\n Создатель бота 👨‍🦱: @dize_dev(Илдырым Денница)\n Достижения🏆: {rewards_list[1]}")
+    # await asyncio.sleep(0.5)
+    await edit_message(
+        message,
+        " ")
 
     await user.api.messages.send(
         peer_id=peer_id,
         random_id=0,
-        sticker_id=79157,
+        attachment="photo-202995525_457239051"
+
 
     )
